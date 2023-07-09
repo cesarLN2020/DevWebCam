6edd42dab7dd9421609e1c70be2ceff84ee7bdb2
Cesar LN <cesarMLGPRO@gmail.com>
2023-07-09 11:54:02 -0600

    registro2

--- views/auth/registro.php
+++ views/auth/registro.php
@@ -4,21 +4,26 @@
     </h2>
     <p class="auth__texto">Registrate en DevWebCamp</p>
 
-    <form action="" class="formulario">
+    <?php 
+        require_once __DIR__.'/../templates/alertas.php';  
+    ?>
+
+
+    <form method="POST" action="/registro" class="formulario">
 
         <div class="formulario__campo">
             <label for="nombre" class="formulario__label">Nombre</label>
-            <input type="text" class="formulario__input" placeholder="Tu Nombre" id="nombre" name="nombre">
+            <input type="text" class="formulario__input" placeholder="Tu Nombre" id="nombre" name="nombre" value="<?php echo $usuario->nombre; ?>">
         </div>
-
+ 
         <div class="formulario__campo">
             <label for="apellido" class="formulario__label">Apellido</label>
-            <input type="text" class="formulario__input" placeholder="Tu Apellido" id="apellido" name="apellido">
+            <input type="text" class="formulario__input" placeholder="Tu Apellido" id="apellido" name="apellido" value="<?php echo $usuario->apellido; ?>">
         </div>
 
         <div class="formulario__campo">
             <label for="email" class="formulario__label">Email</label>
-            <input type="email" class="formulario__input" placeholder="Tu Email" id="email" name="email">
+            <input type="email" class="formulario__input" placeholder="Tu Email" id="email" name="email" value="<?php echo $usuario->email; ?>">
         </div>
       
         <div class="formulario__campo">
@@ -28,7 +33,7 @@
 
         <div class="formulario__campo">
             <label for="password2" class="formulario__label">Repetir Password</label>
-            <input type="password2" class="formulario__input" placeholder="Repetir password" id="password2" name="password2">
+            <input type="password" class="formulario__input" placeholder="Repetir password" id="password2" name="password2">
         </div>
 
         <input type="submit" class="formulario__submit" value="Iniciar Sesión">
